c5e92f9892eb2318bdc4c466e55d1400e743832f
amazonsx <465290180@qq.com>
2014-06-08 19:36:45 +0800

    new task: LRU cache

---
+++ leetcode/lru_cache/cpp/lru.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class LRUCache{
+	public:
+	    LRUCache(int capacity) {
+	    }
+	    int get(int key) {
+		}
+	   	void set(int key, int value) { 
+		}
+};
+int main(int argv, char *argv[]){
+	return 1;
+}
