7d9deb2ea3f95da701399c89f4693f53a990f849
amazonsx <465290180@qq.com>
2014-06-28 23:39:22 +0800

    TODO tomorrow, Word Break & Word Break II

---
+++ leetcode/word_break/cpp/word_break.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#define DEBUG
+using namespace std;
+
+class Solution {
+	public:
+		bool wordBreak(string s, unordered_set<string> &dict);
+};
+
+int main(int argc, char *argv[]) {
+	return 1;
+}
---
+++ leetcode/word_break_ii/cpp/word_break.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#define DEBUG
+using namespace std;
+
+class Solution {
+	public:
+		vector<string> wordBreak(string s, unordered_set<string> &dict);
+};
+
+int main(int argc, char *argv[]) {
+	return 1;
+}
