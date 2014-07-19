c47945fef2c292ee5ef0a85029aee6d47ea80d37
amazonsx <465290180@qq.com>
2014-07-19 11:04:49 +0800

    cpp implementation of Word Break II

--- leetcode/word_break_II/cpp/solution.cpp
+++ leetcode/word_break_II/cpp/solution.cpp
@@ -6,63 +6,65 @@
 #define DEBUG
 using namespace std;
 
-struct TreeNode{
+struct WordTreeNode{
     unsigned start;
     int len;
-    vector<TreeNode*> descendants;
-    TreeNode():start(0),len(-1){}
-    TreeNode(unsigned start, int len):start(start), len(len) {}
+    vector<WordTreeNode*> descendants;
+    WordTreeNode():start(0),len(-1){}
+    WordTreeNode(unsigned int start, int len):start(start), len(len) {}
 };
 
 class Solution {
 private:
     bool splitWords(string s, unsigned start, unordered_set<string> &dict, 
-            map<char, vector<unsigned int> > dict_data, TreeNode *head, map<int, map<int, TreeNode*> > &mem_nodes );
-    void BFS( TreeNode *head, string s, map<TreeNode*, unordered_set<string> > &mem_str);
-    void init( map<char, vector<unsigned int> > &dict_data, unordered_set<string> &dict);
+            map<char, unordered_set<unsigned int> > &dict_data, WordTreeNode *head, map<int, map<int, WordTreeNode*> > &mem_nodes );
+    void BFS( WordTreeNode *head, string s, map<WordTreeNode*, unordered_set<string> > &mem_str, 
+            vector<string> &res, string formerStr);
+    void init( map<char, unordered_set<unsigned int> > &dict_data, unordered_set<string> &dict);
 public:
     vector<string> wordBreak(string s, unordered_set<string> &dict);
-};
+}i;
 
-void Solution::BFS( TreeNode *head, string s, map<TreeNode*, unordered_set<string> > &mem_str) {
-    if ( head->descendants.size() == 0) { 
-        mem_str[head].insert(s.substr(head->start, head->len));
+void Solution::BFS( WordTreeNode *head, string s, map<WordTreeNode*, unordered_set<string> > &mem_str, 
+        vector<string> &res, string formerStr) {
+    if (mem_str.find(head) != mem_str.end()) {
+        for ( unordered_set<string>::iterator iter = mem_str[head].begin();
+                iter != mem_str[head].end(); iter ++) {
+            cout << *iter << endl;
+            res.push_back(formerStr + *iter);    
+        }
         return;
     }
-    if (mem_str.find(head) != mem_str.end())    return;
-    string current("");     
-    if (head->len > 0)
-        current = s.substr(head->start, head->len) + " ";
+    string currentStr("");
+    if ( head->descendants.size() == 0) { 
+        currentStr = s.substr(head->start, head->len);
+        mem_str[head].insert(currentStr);
+        res.push_back( formerStr + currentStr);
+        return;
+    }
+    // head may be the root which is empty
+    if (head->len != -1)
+        currentStr = s.substr(head->start, head->len) + " ";
 
     for ( unsigned int i = 0; i < head->descendants.size(); i ++) {
-        BFS( head->descendants[i], s, mem_str);
+        BFS( head->descendants[i], s, mem_str, res, formerStr + currentStr);
         for ( unordered_set<string>::iterator iter = mem_str[head->descendants[i]].begin();
                 iter != mem_str[head->descendants[i]].end(); iter ++) {
-            string tmp = current + *iter;
-            mem_str[head].insert(tmp);
+            mem_str[head].insert(currentStr + *iter);
         }
     }
 }
 
-void Solution::init( map<char, vector<unsigned int> > &dict_data, unordered_set<string> &dict) {
+void Solution::init( map<char, unordered_set<unsigned int> > &dict_data, unordered_set<string> &dict) {
     for ( unordered_set<string>::iterator iter = dict.begin();
             iter != dict.end(); iter ++) {
-        if ( dict_data[(*iter)[0]].size() == 0){
-            dict_data[(*iter)[0]].push_back(iter->size());
-            dict_data[(*iter)[0]].push_back(iter->size());
-        } else {
-            if ( iter->size() < dict_data[(*iter)[0]][0]) {
-                dict_data[(*iter)[0]][0] = iter->size();
-            } else if (iter->size() > dict_data[(*iter)[0]][1]) {
-                dict_data[(*iter)[0]][1] = iter->size();
-            }
-        }
+        dict_data[(*iter)[0]].insert((*iter).size());
     }
 }
 
 bool Solution::splitWords(string s, unsigned start, unordered_set<string> &dict, 
-        map<char, vector<unsigned int> > dict_data, TreeNode *head, 
-        map<int, map<int, TreeNode*> > &mem_nodes) {
+        map<char, unordered_set<unsigned int> > &dict_data, WordTreeNode *head, 
+        map<int, map<int, WordTreeNode*> > &mem_nodes) {
     // corner case: start = s.size() = 0
     if (start == s.size())  {
         return true;
@@ -73,77 +75,85 @@ bool Solution::splitWords(string s, unsigned start, unordered_set<string> &dict,
     if (dict_data.find(s[start]) == dict_data.end()) {
         return false;
     }
-    unsigned int len = dict_data[s[start]][0];
     bool res = false;
-    while( len <= dict_data[s[start]][1]) {
-        if ((start + len) > s.size())   break;
+    for ( unordered_set<unsigned int>::iterator iter = dict_data[s[start]].begin();
+            iter != dict_data[s[start]].end(); iter ++) {
+        int len = (signed)(*iter);
+        if ( (start + len) > s.size())  continue;
         string current = s.substr(start, len);
-        if ( dict.find(current) != dict.end()) {
-            if (mem_nodes.find(start) != mem_nodes.end()) {
-                if (mem_nodes[start].find(len) != mem_nodes[start].end()) {
-                    if (mem_nodes[start][len] != NULL){
-                        head->descendants.push_back(mem_nodes[start][len]);
-                        res = true;
-                    }
-                    len ++;
-                    continue;
+        if (dict.find(current) == dict.end()) continue;
+        if (mem_nodes.find(start) != mem_nodes.end()) {
+            if (mem_nodes[start].find(len) != mem_nodes[start].end()) {
+                if (mem_nodes[start][len] != NULL) {
+                    head->descendants.push_back(mem_nodes[start][len]);
+                    res = true;
                 }
-            } 
-            TreeNode *currentNode = new TreeNode( start, len);
-            bool cur_res = splitWords( s, start + len, dict, dict_data, currentNode, mem_nodes);
-            if (cur_res) {
-                head->descendants.push_back( currentNode);
-                mem_nodes[start][len] =  currentNode;
-            } else {
-                mem_nodes[start][len] =  NULL;
-                delete currentNode;
+                continue;
             }
-            res |= cur_res;
         }
-        len ++;
+        WordTreeNode *currentNode = new WordTreeNode( start, len);
+        if ((start+len) == s.size()) {
+            head->descendants.push_back(currentNode);
+            res = true;
+            continue;
+        }
+        bool current_res = splitWords( s, start+len, dict, dict_data, currentNode, mem_nodes);
+        if (current_res) {
+            head->descendants.push_back(currentNode);
+            mem_nodes[start][len] = currentNode;
+        } else {
+            delete currentNode;
+            mem_nodes[start][len] = NULL;
+        }
+        res |= current_res;
     }
-    
-    if (!res) return false;
-    return true;
+    return res;    
 }
 
 vector<string> Solution::wordBreak(string s, unordered_set<string> &dict) {
-    map<char, vector<unsigned int> >  dict_data;
+    vector<string> res;
+    if (dict.size() == 0)   return res;
+    map<char, unordered_set<unsigned int> >  dict_data;
     init( dict_data, dict);
 
-    vector<string> res;
-    if (dict_data.size() == 0) {
-        return res;
-    }
-    TreeNode head;
-    map<int, map<int, TreeNode*> > mem_nodes;
+    WordTreeNode head;
+    // map< start, map< len, WordTreeNode*> > 
+    map<int, map<int, WordTreeNode*> > mem_nodes;
     bool avail = splitWords(s, 0, dict, dict_data, &head, mem_nodes);
     if (!avail) return res;
 
-    map<TreeNode*, unordered_set<string> > mem_str;
-    BFS( &head, s, mem_str);
-    for ( unsigned i = 0; i < head.descendants.size(); i ++) {
-        res.insert(res.end(), mem_str[head.descendants[i]].begin(), mem_str[head.descendants[i]].end());
-    }
+    map<WordTreeNode*, unordered_set<string> > mem_str;
+    BFS( &head, s, mem_str, res, "");
     return res;
 }
 
 int main(int argc, char *argv[]) {
     Solution s;
     /*
-    string str("catsanddog");
-    unordered_set<string> dict({"cat", "cats", "and", "sand", "dog"});
-    string str("a");
-    unordered_set<string> dict({"b"});
-    string str("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab");
-    unordered_set<string> dict({"aaaaaaaaaaa", "aaaaaa", "b"});
-    string str("ab");
-    unordered_set<string> dict({"a", "b"});
     string str("aaaaaaa");
     unordered_set<string> dict({"aaaa", "aa"});
-    */
+
+    string str("catsanddog");
+    unordered_set<string> dict({"cat", "cats", "and", "sand", "dog"});
+
+    string str("a");
+    unordered_set<string> dict({"b"});
+
+    string str("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab");
+    unordered_set<string> dict({"aaaaaaaaaaa", "aaaaaa", "b"});
+
+    string str("ab");
+    unordered_set<string> dict({"a", "b"});
+
     string str("a");
     unordered_set<string> dict({"a"});
+
+    string str("ab");
+    unordered_set<string> dict({"a", "b"});
+    */
+
+    string str("aaaaaaa");
+    unordered_set<string> dict({"aaaa","aa","a"});
 
     struct timeval first;
     gettimeofday(&first, NULL);
@@ -152,7 +162,7 @@ int main(int argc, char *argv[]) {
 
     struct timeval second;
     gettimeofday(&second, NULL);
-    cout << "Total time" << 1000000 * (second.tv_sec - first.tv_sec ) + second.tv_usec - first.tv_usec << endl;
+    cout << "Total time: " << 1000000 * (second.tv_sec - first.tv_sec ) + second.tv_usec - first.tv_usec << endl;
 
     cout << "Res size is " << res.size() << endl;
     for ( unsigned int i = 0; i < res.size(); i ++ ) {
