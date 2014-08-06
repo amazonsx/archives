ea98834b76b92f61c9267a505b90701573a58fc2
amazonsx <465290180@qq.com>
2014-08-06 21:59:34 +0800

    TODO: 4 sum

---
+++ leetcode/4_sum/cpp/solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#define DEBUG
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int> > fourSum(vector<int> &num, int target);
+};
+
+vector<vector<int> > Solution::fourSum(vector<int> &num, int target) {
+}
+
+int main(int argc, char *argv[]) {
+	return 1;
+}
