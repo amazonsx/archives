8ab0899a66d75cb6057d3c0733558322e8e4782d
amazonsx <465290180@qq.com>
2014-08-10 22:43:37 +0800

    TODO: Permutations II

---
+++ leetcode/permutations_II/cpp/solution.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#define DEBUG
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int> > permuteUnique( vector<int> &num);
+};
+
+vector<vector<int> > permuteUnique( vector<int> &num) {
+    
+}
+
+int main(int argc, char *argv[]) {
+	return 1;
+}
