9a29536669116894fd3a433519c90837934cc2b5
amazonsx <465290180@qq.com>
2014-08-10 22:38:40 +0800

    cpp implemenataion of Distinct Subsequences

--- leetcode/distinct_subsequences/cpp/solution.cc
+++ leetcode/distinct_subsequences/cpp/solution.cc
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <map>
+#include <queue>
 #define DEBUG
 using namespace std;
 
 class Solution {
+private:
+    bool matchSubseq(string S, string T, int posS, int posT);
 public:
+    int numDistinct( string S, string T);
 };
+
+int Solution::numDistinct( string S, string T) {
+    if (S.size() < T.size())    return 0;
+    if (S.size() == T.size()) {
+        if (S == T) return 1;
+        else    return 0;
+    }
+    int diff = S.size() - T.size();
+        
+}
 
 int main(int argc, char *argv[]) {
 	return 1;
