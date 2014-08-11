e18119aaa2f81422dc4d2abb06601efdfa953199
amazonsx <465290180@qq.com>
2014-08-11 15:38:34 +0800

    TODO: Palindrome Partitioning

---
+++ leetcode/palindrome_partitioning/cpp/solution.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#define DEBUG
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string> > partition(string s);
+};
+
+vector<vector<string> > Solution::partition(string s) {
+      
+}
+
+int main(int argc, char *argv[]) {
+	return 1;
+}
