3cc114bfce47cc8756744ab6dbc2b0eb00ff5a35
amazonsx <465290180@qq.com>
2014-08-11 20:43:31 +0800

    TODO: gray code

---
+++ leetcode/gray_code/cpp/solution.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#define DEBUG
+using namespace std;
+
+class Solution {
+public:
+    vector<int> grayCode(int n);
+};
+
+vector<int> Solution::grayCode(int n) {
+    vector<int> res;
+    if (n == 0) return res;
+}
+
+int main(int argc, char *argv[]) {
+	return 1;
+}
