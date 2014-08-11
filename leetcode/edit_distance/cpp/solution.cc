5edfc66eeddf4bc72365453346c3afbfdb9948e9
amazonsx <465290180@qq.com>
2014-08-11 15:29:41 +0800

    TODO: Edit Distance

---
+++ leetcode/edit_distance/cpp/solution.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#define DEBUG
+using namespace std;
+
+class Solution {
+public:
+    int minDistance(string word1, string word2);
+};
+
+int Solution::minDistance(string word1, string word2) {
+
+}
+
+int main(int argc, char *argv[]) {
+	return 1;
+}
