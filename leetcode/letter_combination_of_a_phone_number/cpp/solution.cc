20ef0cfff86f63c3c7869d2b96bd94115583dd45
amazonsx <465290180@qq.com>
2014-08-15 20:10:22 +0800

    TODO: Letter Combination of A Phone Number

---
+++ leetcode/letter_combination_of_a_phone_number/cpp/solution.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#define DEBUG
+using namespace std;
+
+class Solution {
+public:
+    vector<string> letterCombinations(string digits);
+};
+
+int main(int argc, char *argv[]) {
+	return 1;
+}
--- leetcode/maximal_rectangle/cpp/solution.cc
+++ leetcode/maximal_rectangle/cpp/solution.cc
@@ -4,7 +4,13 @@ using namespace std;
 
 class Solution {
 	public:
+        int maximalRectangle( vector<vector<char> > &matrix);
 };
+
+int Solution::maximalRectangle( vector<vector<char> > &matrix) {
+
+}
+
 
 int main(int argc, char *argv[]) {
 	return 1;
