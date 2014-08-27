eff21a4fa699c2f048adc2ce0b23dc089dd9b284
amazonsx <465290180@qq.com>
2014-08-27 20:56:02 +0800

    cpp implementation of Subsets II

--- leetcode/subsets_II/cpp/solution.cc
+++ leetcode/subsets_II/cpp/solution.cc
@@ -1,14 +1,14 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <vector>
 #define DEBUG
 using namespace std;
 
 class Solution {
-private:
-    void quicksort(vector<int> &S, int left, int right);
-public:
-    vector<vector<int> > subsetsWithDup(vector<int> &S);
+    private:
+        void quicksort(vector<int> &S, int left, int right);
+    public:
+        vector<vector<int> > subsetsWithDup(vector<int> &S);
 };
 
 void Solution::quicksort( vector<int> &S, int left, int right) {
@@ -25,29 +25,53 @@ void Solution::quicksort( vector<int> &S, int left, int right) {
         j ++;
     }
     S[right] = S[++i];
-    S[i] = S[right];
+    S[i] = pivot;
     quicksort(S, left, i - 1);
-    quicksort(S, i+1, right);
-    
+    quicksort(S, i + 1, right);
 }
 
 vector<vector<int> > Solution::subsetsWithDup(vector<int> &S) {     
-    vector<vector<int> > res;
-    vector<int> empty_set;
-    res.push_back(empty_set);
-
-    quicksort(S, 0, S.size());
-
-    map< int, int> intMap;
-    for ( int i = 0; i < (signed)S.size(); i ++){
-        intMap[S[i]] ++;
+    vector<vector<int> > res(1, vector<int>());
+    quicksort(S, 0, S.size()-1);
+    vector<vector<vector<vector<int> > > > subsets(S.size(), 
+            vector<vector<vector<int> > >(S.size()+1, vector<vector<int> >()));
+    for ( int i = 0; i < (signed)S.size(); i ++) {
+        subsets[i][1].push_back(vector<int>(1, S[i]));
+        if ( i > 0 ) {
+            if ( S[i] == S[i-1])    continue;
+        }
+        res.push_back(subsets[i][1].back());
     }
-        
-    for ( int i = 0; i < (signed); ){
+    for ( int i = 2; i <= (signed)S.size(); i ++) {
+        for ( int j = 0; j < (signed)S.size(); j ++) {
+            for ( int k = j+1; k < (signed)S.size(); k ++) {
+                if ( (k > j+1) && (S[k] == S[k-1]))    continue;
+                for ( int m = 0; m < (signed)subsets[k][i-1].size(); m ++) {
+                    subsets[j][i].push_back(vector<int>(1, S[j]));
+                    subsets[j][i].back().insert(subsets[j][i].back().end(), 
+                            subsets[k][i-1][m].begin(), subsets[k][i-1][m].end());
+                    if ( j > 0) {
+                        if ( S[j] == S[j-1])    continue;
+                    }
+                    res.push_back(subsets[j][i].back());
+                }
+            }
+        }
     }
+    return res;
 }
 
 
 int main(int argc, char *argv[]) {
-	return 1;
+    int arr[] = {2, 1, 2, 1};
+    vector<int> S(arr, arr + sizeof(arr)/sizeof(int));
+    Solution s;
+    vector<vector<int> > res = s.subsetsWithDup(S);
+    for ( int i = 0; i < (signed)res.size(); i ++) {
+        for ( int j = 0; j < (signed)res[i].size(); j ++) {
+            cout << res[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 1;
 }
